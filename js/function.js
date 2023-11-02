80f1409789aa4513f7096e53a733baccbb0ca035
unknown <sohagrana2311@gmail.com>
2023-11-02 20:19:44 +0600

    frist-commit

---
+++ js/function.js
@@ -0,0 +1,6 @@
+
+const menuBer= document.getElementById("menu-bar");
+menuBer.addEventListener('click', () =>{
+    const navBer = document.getElementById('nav-bar')
+    navBer.classList.toggle('active')
+})
